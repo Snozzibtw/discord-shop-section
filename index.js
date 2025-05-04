e69b6d4590a43a155a68bb1bfaaae7f983779c31
Snozzi <99423719+Snozzibtw@users.noreply.github.com>
2025-05-04 20:15:55 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,79 +1,93 @@
 let gridContainer = document.getElementById("gridContainer")
 
-let shopItem1 = document.getElementById("shop1")
-let shopItem2 = document.getElementById("shop2")
-let shopItem3 = document.getElementById("shop3")
 
-let shop1H2 = document.getElementById("shop1H2")
-let shop2H2 = document.getElementById("shop2H2")
-let shop3H2 = document.getElementById("shop3H2")
+let shop1H2 = document.getElementById("itemShopFirstColHeading2")
+let shop2H2 = document.getElementById("itemShopSecondColHeading2")
+let shop3H2 = document.getElementById("itemShopThirdColHeading2")
 
-let shopBTN1 = document.getElementById("shop1BTN")
-let shopBTN2 = document.getElementById("shop2BTN")
-let shopBTN3 = document.getElementById("shop3BTN")
+let shopBTN1 = document.getElementById("itemShopFirstColButton")
+let shopBTN2 = document.getElementById("itemShopSecondColButton")
+let shopBTN3 = document.getElementById("itemShopThirdColButton")
 
-let shopImage1 = document.getElementById("shop1Image")
-let shopImage2 = document.getElementById("shop2Image")
-let shopImage3 = document.getElementById("shop3Image")
+let shopImage1 = document.getElementById("itemShopFirstColImage")
+let shopImage2 = document.getElementById("itemShopSecondColImage")
+let shopImage3 = document.getElementById("itemShopThirdColImage")
 
 let checkout = document.getElementById("checkout")
 let exitP = document.getElementById("exit")
+let checkoutConfirm = document.getElementById("checkoutConfirmButton")
+let thankYouPopUp = document.getElementById("thankyouPurchase")
+let exitButton = document.getElementById("exitButton");
 
 exitP.addEventListener("click", () => {
     checkout.style.display = "none";
     gridContainer.style.filter = "none";
 });
 
-// Buy button applies blur and shows checkout
+
 function buyBTN(bundleName, bundlePrice) {
-    checkout.style.display = "block";
-    gridContainer.style.filter = "blur(5px)";
-    
-    document.getElementById("purchaseDetailShopBudnle").innerText = bundleName;
-    document.getElementById("purchaseDetailShopPrice").innerText = bundlePrice;
+  checkout.style.display = "block";
+  gridContainer.style.filter = "blur(5px)";
+  
+  document.getElementById("purchaseDetailShopBudnle").innerText = bundleName;
+  document.getElementById("purchaseDetailShopPrice").innerText = bundlePrice;
 }
 
 fetch('data.json')
-  .then(response => response.json())
-  .then(data => {
-
-    let productName1 = data["Shop1-Data"][0].name
-    let productName2 = data["Shop2-Data"][0].name
-    let productName3 = data["Shop3-Data"][0].name
-
-    let productPrice1 = data["Shop1-Data"][0].price
-    let productPrice2 = data["Shop2-Data"][0].price
-    let productPrice3 = data["Shop3-Data"][0].price
-    
-    let imageShop1 = data["Shop1-Data"][0].image
-    let imageShop2 = data["Shop2-Data"][0].image
-    let imageShop3 = data["Shop3-Data"][0].image
-
-    shop1H2.textContent = productName1
-    shop2H2.textContent = productName2
-    shop3H2.textContent = productName3
-
-    shopBTN1.textContent += " " + productPrice1
-    shopBTN2.textContent += " " + productPrice2    
-    shopBTN3.textContent += " " + productPrice3
-
-    shopImage1.src = imageShop1;
-    shopImage2.src = imageShop2;
-    shopImage3.src = imageShop3;
-
-   shopBTN1.addEventListener("click", () => {
-      buyBTN(productName1, productPrice1);
-    });
-
-    shopBTN2.addEventListener("click", () => {
-        buyBTN(productName2, productPrice2);
-    });
-
-    shopBTN3.addEventListener("click", () => {
-        buyBTN(productName3, productPrice3);
-    });
+.then(response => response.json())
+.then(data => {
+  
+  let starWarsBundleName = data["StarWars-Bundle-Shop"][0].name
+  let starWarsHyperSpaceName = data["StarWars-HyperSpace-Shop"][0].name
+  let starWarsR2D2Name = data["StarWars-R2D2-Shop"][0].name
+  
+  let starWarsBundlePrice = data["StarWars-Bundle-Shop"][0].price
+  let starWarsHyperSpacePrice = data["StarWars-HyperSpace-Shop"][0].price
+  let starWarsR2D2Price = data["StarWars-R2D2-Shop"][0].price
+  
+  let starWarsBundleImage = data["StarWars-Bundle-Shop"][0].image
+  let starWarsHyperSpaceImage = data["StarWars-HyperSpace-Shop"][0].image
+  let starWarsR2D2Image = data["StarWars-R2D2-Shop"][0].image
+  
+  shop1H2.textContent = starWarsBundleName
+  shop2H2.textContent = starWarsHyperSpaceName
+  shop3H2.textContent = starWarsR2D2Name
+  
+  shopBTN1.textContent += " " + starWarsBundlePrice
+  shopBTN2.textContent += " " + starWarsHyperSpacePrice
+  shopBTN3.textContent += " " + starWarsR2D2Price
+  
+  shopImage1.src = starWarsBundleImage;
+  shopImage2.src = starWarsHyperSpaceImage;
+  shopImage3.src = starWarsR2D2Image;
+  
+  shopBTN1.addEventListener("click", () => {
+    buyBTN(starWarsBundleName, starWarsBundlePrice);
   })
-  .catch(error => {
-    console.error('Error loading JSON:', error)
+  shopBTN2.addEventListener("click", () => {
+    buyBTN(starWarsHyperSpaceName, starWarsHyperSpacePrice);
+  })
+  shopBTN3.addEventListener("click", () => {
+    buyBTN(starWarsR2D2Name, starWarsR2D2Price);
+  })
+  
+})
+.catch(error => {
+  console.error('Error loading JSON:', error)
 })
 
+checkoutConfirm.addEventListener("click", () => {
+  if (checkout.style.display === "block") {
+      checkout.style.display = "none"
+      thankYouPopUp.style.display = "block"
+  }
+})
+
+function showThankYouMessage() {
+  thankyouPurchase.style.display = "block";
+}
+
+exitButton.addEventListener("click", () => {
+  thankyouPurchase.style.display = "none";
+  gridContainer.style.filter = "none";
+});
